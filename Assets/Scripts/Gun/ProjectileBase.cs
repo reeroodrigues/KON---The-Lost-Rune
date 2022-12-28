fdd133f5b16aa02d8abede57970a19f50ddd1bdf
Renan Malveira Rodrigues <renan.pocket@gmail.com>
2022-12-28 11:56:15 -0300

    Kon - The Lost Rune prototype

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -20,6 +20,13 @@ public class ProjectileBase : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-       
+        var damageable = collision.transform.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.Damage(damageAmount);
+        }
+
+        Destroy(gameObject);
     }
 }
